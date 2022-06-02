698f0e96bcd609fbeab66f936b308148609ff85a
Noverry Ambo <noverryambo@gmail.com>
2022-06-02 09:47:04 +0700

    edit and rename

--- script.js
+++
@@ -1,6 +0,0 @@
-let nilaiAwal = 1;
-
-while (nilaiAwal <= 10) {
-    console.log("Angkot no. " + nilaiAwal + " beroperasi dengan baik")
-    nilaiAwal++;
-}
---
+++ tugas4.js
@@ -0,0 +1,83 @@
+let angkot = 1;
+let maxAngkot = 10;
+
+for (angkot; angkot <= maxAngkot; angkot++) {
+  if (angkot <= 6) {
+    console.log("Angkot No. " + angkot + " beroperasi dengan baik.");
+  } else if (angkot === 8) {
+    console.log("Angkot No. " + angkot + " sedang lembur.");
+  } else {
+    console.log("Angkot No. " + angkot + " sedang tidak beroperasi.");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let jumlahAngkot = 10;
+// let angkotBeroperasi = 6;
+
+
+// for(let noAngkot = 1; noAngkot <= 10; noAngkot++){
+    
+//     if(noAngkot == 8){
+//         console.log("Angkot No. "+ noAngkot + " sedang lembur")
+//     }else if(noAngkot >= 1 && noAngkot <= 6){
+//         console.log("Angkot No. "+ noAngkot + " beroperasi dengan baik")
+//     }else(noAngkot >= 7 || noAngkot <= 10 ) 
+//     {
+//         console.log("Angkot No. "+ noAngkot + " sedang tidak beroperasi")
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+// if (noAngkot <=6)
+// {
+// console.log("Ankgot No. " + noAngkot + " beroperasi dengan baik")
+// }else if(noAngkot <= 10 && noAngkot >= 6)
+// {
+// console.log("Ankgot No. " + noAngkot + " sedang tidak beroperasi dengan baik")
+// }else if(noAngkot == 8)
+// {
+//     console.log("Angkot sedang lembur")
+// } 
+
+
+
+
+
+// while (nilaiAwal <= 10) {
+//     console.log("Angkot no. " + nilaiAwal + " beroperasi dengan baik")
+//     nilaiAwal++;
+// }
